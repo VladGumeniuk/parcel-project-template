65f34685b5a31df5355b3c8b88ab7b47559fa4e1
Pavel Denisov <denisov.p.p@gmail.com>
2021-02-03 23:01:55 +0200

    Update slider.js

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,3 +1,11 @@
+// $('.review-slider').slick({
+//     dots: true
+// });
+
 $('.review-slider').slick({
-    dots: true
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+    autoplaySpeed: 4000,
+  dots: true
 });

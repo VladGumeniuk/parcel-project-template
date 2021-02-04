058fc3e96c4591d651f600b41950188a1e3430dd
VladGumeniuk <76571048+VladGumeniuk@users.noreply.github.com>
2021-02-04 10:12:47 +0200

    Updates

--- menu.0c91648c.js
+++ menu.0c91648c.js
@@ -157,7 +157,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "13294" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "14778" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};

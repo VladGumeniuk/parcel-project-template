a0907a28e3e54a5812d68b61856c0d68feb0079d
MrPaul007 <75275133+MrPaul007@users.noreply.github.com>
2021-02-04 19:03:47 +0200

    fixed js file

--- src/js/locations-map.js
+++ src/js/locations-map.js
@@ -1,8 +1,8 @@
 (() => {
     const refs = {
-      openModalBtn: document.querySelector('[data-modal-open]'),
-      closeModalBtn: document.querySelector('[data-modal-close]'),
-      modal: document.querySelector('[data-modal]'),
+      openModalBtn: document.querySelector('[data-location-modal-open]'),
+      closeModalBtn: document.querySelector('[data-location-modal-close]'),
+      modal: document.querySelector('[data-location-modal]'),
     };
   
     refs.openModalBtn.addEventListener('click', toggleModal);

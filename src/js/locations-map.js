ec47f5d4e5b9a68346ddddd26a34c2048091a8fe
MrPaul007 <75275133+MrPaul007@users.noreply.github.com>
2021-02-04 18:13:41 +0200

    added modal

---
+++ src/js/locations-map.js
@@ -0,0 +1,36 @@
+(() => {
+    const refs = {
+      openModalBtn: document.querySelector('[data-modal-open]'),
+      closeModalBtn: document.querySelector('[data-modal-close]'),
+      modal: document.querySelector('[data-modal]'),
+    };
+  
+    refs.openModalBtn.addEventListener('click', toggleModal);
+    refs.closeModalBtn.addEventListener('click', toggleModal);
+  
+    function toggleModal() {
+      refs.modal.classList.toggle('is-hidden');
+    }
+  })();
+
+  const srcMap = {
+    chicago: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d380513.7159859942!2d-88.01214778988322!3d41.83339250495681!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x880e2c3cd0f4cbed%3A0xafe0a6ad09c0c000!2z0KfQuNC60LDSkdC-LCDQhtC70LvRltC90L7QudGBLCDQodC_0L7Qu9GD0YfQtdC90ZYg0KjRgtCw0YLQuCDQkNC80LXRgNC40LrQuA!5e0!3m2!1suk!2sua!4v1612441729294!5m2!1suk!2sua',
+    losAngeles: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d423286.27405770525!2d-118.69192047471653!3d34.02016130653294!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80c2c75ddc27da13%3A0xe22fdf6f254608f4!2z0JvQvtGBLdCQ0L3QtNC20LXQu9C10YEsINCa0LDQu9GW0YTQvtGA0L3RltGPLCDQodC_0L7Qu9GD0YfQtdC90ZYg0KjRgtCw0YLQuCDQkNC80LXRgNC40LrQuA!5e0!3m2!1suk!2sua!4v1612446545674!5m2!1suk!2sua',
+    newYork: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d193595.15830869428!2d-74.119763973046!3d40.69766374874431!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2z0J3RjNGOLdCZ0L7RgNC6LCDQodC_0L7Qu9GD0YfQtdC90ZYg0KjRgtCw0YLQuCDQkNC80LXRgNC40LrQuA!5e0!3m2!1suk!2sua!4v1612446606599!5m2!1suk!2sua'
+  }
+
+  const iframe = document.getElementById('iframe-map');
+  const locationButtons = document.getElementsByClassName('location-btn');
+  iframe.setAttribute('src', srcMap.chicago);
+  for (const btn of locationButtons) {
+    btn.addEventListener('click', () => {
+      iframe.setAttribute('src', srcMap[btn.dataset.location]);
+
+      for (const btn of locationButtons) {
+          btn.classList.remove('active');
+      }
+      
+      btn.classList.add('active');
+    });
+  }
+
